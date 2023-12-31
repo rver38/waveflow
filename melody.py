b452fa5edf70ccca897eef6f2e28cdb47de58b78
rver <83334985+rver38@users.noreply.github.com>
2023-12-31 01:16:26 +0100

    Add files via upload

---
+++ melody.py
@@ -0,0 +1,8 @@
+from waveflow.main import Tone,wvtools
+import matplotlib.pyplot as plt
+
+a = Tone(wvtools.freq_fade(40,20),1,wvtools.amp(1))
+print(a.fq[0])
+plt.plot(a.create())
+plt.show()
+wvtools.play(a)
